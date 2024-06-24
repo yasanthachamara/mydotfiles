c2a8386185c0241956856fe2eb09c4ecb17cff1b
Yasantha <yasantha666@gmail.com>
2024-06-25 00:56:52 +0800

    Add fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,37 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+
+    # Better shell history
+    atuin init fish | source
+end
+
+# Starship prompt
+starship init fish | source
+
+# Zoxide
+zoxide init fish | source
+
+# Use fd instead of find
+export FZF_DEFAULT_COMMAND="fd -H --strip-cwd-prefix --exclude .git"
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_ALT_C_COMMAND="fd --type=d -H --strip-cwd-prefix --exclude .git"
+
+# Set up fzf key bindings
+fzf --fish | source
+
+# Aliases
+alias c="clear"
+alias cat="bat"
+alias v="nvim"
+alias vim="nvim"
+alias ld="eza -lghD"
+alias lf="eza -lghF --color=always | grep -v /"
+alias lh="eza -dlgh .* --group-directories-first"
+alias ll="eza -lgh --group-directories-first --color=always --icons=always"
+alias lla="eza -lgha --group-directories-first --color=always --icons=always"
+alias ls="eza -G --group-directories-first --color=always --icons=always --sort=name"
+alias lsa="eza -Ga --group-directories-first --color=always --icons=always --sort=name"
+alias lt="eza -T --color=always --icons=always"
+alias find="fd"
+alias cd="z"
+alias git-bare="git --git-dir=$HOME/mydotfiles --work-tree=$HOME"
